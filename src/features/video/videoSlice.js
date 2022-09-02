0eca5afd2a2cbc9f1f54b8316c3b917169ed7981
Mamun Ahmed <mamunahmedusa@gmail.com>
2022-09-02 11:40:30 +0600

    added like and unlike pending and rejected

--- src/features/video/videoSlice.js
+++ src/features/video/videoSlice.js
@@ -50,6 +50,12 @@ const initialState = {
   loading: false,
   isError: false,
   error: "",
+  loadingLikes: false,
+  isErrorLikes: false,
+  errorLikes: "",
+  loadingUnlikes: false,
+  isErrorUnlikes: false,
+  errorUnlikes: "",
 };
 
 const videoSlice = createSlice({
@@ -71,11 +77,33 @@ const videoSlice = createSlice({
         state.isError = true;
         state.error = action.error?.message;
       })
+      .addCase(likeAsync.pending, (state, action) => {
+        state.isErrorLikes = false;
+        state.loadingLikes = true;
+      })
       .addCase(likeAsync.fulfilled, (state, action) => {
+        state.loadingLikes = false;
         state.video.likes = action.payload.likes;
       })
+      .addCase(likeAsync.rejected, (state, action) => {
+        state.loadingLikes = false;
+        state.video.likes = [];
+        state.isErrorLikes = true;
+        state.errorLikes = action.error?.message;
+      })
+      .addCase(unLikeAsync.pending, (state, action) => {
+        state.isErrorUnlikes = false;
+        state.loadingUnlikes = true;
+      })
       .addCase(unLikeAsync.fulfilled, (state, action) => {
+        state.loadingUnlikes = false;
         state.video.unlikes = action.payload.unlikes;
+      })
+      .addCase(unLikeAsync.rejected, (state, action) => {
+        state.loadingUnlikes = false;
+        state.video.unlikes = [];
+        state.isErrorUnlikes = true;
+        state.errorUnlikes = action.error?.message;
       });
   },
 });
